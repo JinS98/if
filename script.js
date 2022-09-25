d4b0e22c1a0df6c5d705dbf675778ddb7e0ad915
JinS98 <110406873+JinS98@users.noreply.github.com>
2022-09-25 12:13:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let card = true
+let money_card = 1100
+
+// if (age >= 18 && age < 20) {
+//   console.log("오토바이 운전이 가능합니다.")
+// } else if (age > 20) {
+//   console.log("운전이 가능합니다.")
+// } else {
+//   console.log("운전이 불가능합니다.")
+// }
+
+if (card == true) {
+  if (money_card >= 1250) {
+    console.log("버스 탑승이 가능합니다")
+  }
+  else {
+    console.log("잔액이 부족합니다.")
+  }
+}
+else {
+  console.log("버스 탑승이 불가능합니다")
+}
